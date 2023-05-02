46531ab8209a1a6c47c900d759775ebdda24ade9
dstanchevici <distanchev@gmail.com>
2023-05-02 09:51:52 -0400

    1.9

---
+++ LoopExercise2.java
@@ -0,0 +1,23 @@
+
+public class LoopExercise2 {
+
+    public static void main (String[] argv)
+    {
+	int[] A = {-1, 1, -3, 2, -5, 3, 4, -4, 5};
+
+
+	int sum = 0;
+
+	// WRITE YOUR LOOP HERE.
+	// Use the ternary operator to add elements only if they are positive.
+
+	for (int i=0; i<A.length; i++) {
+	    int addend = (A[i] >= 0) ? A[i] : 0;
+	    sum += addend;
+	}
+
+	System.out.println (sum);
+	
+    }
+
+}
