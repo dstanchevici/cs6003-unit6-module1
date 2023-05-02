965d9c84a1aa12e3e48b0cbc48dd459489467a6e
dstanchevici <distanchev@gmail.com>
2023-05-02 08:48:46 -0400

    1.6

---
+++ Ex_1_6.java~
@@ -0,0 +1,9 @@
+public class Ex_1_6 {
+
+    public static void main (String[] argv)
+    {
+	int k = 130;
+	byte b = (byte) k;
+	System.out.println (b);
+    }
+}
---
+++ LetterOrdinal.java
@@ -0,0 +1,16 @@
+
+public class LetterOrdinal {
+
+    public static void main (String[] argv)
+    {
+	System.out.println ("e: " + ordinal('e'));
+	System.out.println ("E: " + ordinal('E'));
+    }
+
+    static int ordinal (char ch)
+    {
+	// WRITE YOUR CODE HERE
+
+    }
+
+}
